b8fb409bd70dac6014ebde789c96af98584295a8
ysskrishna <sivasaikrishnassk@gmail.com>
2025-06-19 19:20:26 +0530

    Siva | add eml files with and without attachment

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+
+def main():
+    print("Email Audit Service Started")
+
+    # # Case1: Email with image attachment + Email without image attachment
+    # email_threads = ["data/email_with_image_attachment.eml", "data/email_without_image_attachment.eml"]
+
+    # Case2: Email with image attachment
+    email_threads = ["data/email_with_image_attachment.eml"]
+
+    # # Case3: Email without image attachment
+    # email_threads = ["data/email_without_image_attachment.eml"]
+
+    print(f"Email threads: {email_threads}")
+
+
+if __name__ == "__main__":
+    main()
