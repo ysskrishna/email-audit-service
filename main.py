930f19b1e78324b3e8ed1fd54ee1d1ce1846e1f8
ysskrishna <sivasaikrishnassk@gmail.com>
2025-06-19 21:12:20 +0530

    Siva | add counters and print statements

--- main.py
+++ main.py
@@ -16,23 +16,37 @@ def main():
     # email_threads = ["data/non_employee_reply_email.eml"]
 
     employee_domain = "@test.com"
-    employee_reply_email_counter = 0
+    total_email_threads_counter = len(email_threads)
+    valid_employee_reply_counter = 0
+    invalid_email_counter = 0
+
+
     
     for email_path in email_threads:
         print(f"\n📄 Processing: {email_path}")
         parser = EmailParser(email_path)
 
         last_reply = parser.extract_last_reply()
-        # EmailParser.print_email(last_reply)
+        # EmailParser.print_email(last_reply)    
 
         # Check if the last reply is from the employee
         if not last_reply['sender'].lower().endswith(employee_domain):
             print(f"⚠ Last reply is not from the employee")
+            invalid_email_counter += 1
             continue
+        
+        # Add a condition to check if the email has attachment ?
+        if not len(last_reply['attachments']) > 0:
+            print(f"⚠ Email does not have attachment")
+            invalid_email_counter += 1
+            continue
+        
+        valid_employee_reply_counter += 1
 
-        employee_reply_email_counter += 1
-    
-    print(f"🔎 Total employee reply emails: {employee_reply_email_counter}")
+    print(f"🔎 Total email threads: {total_email_threads_counter}")
+    print(f"🔎 Valid employee reply: {valid_employee_reply_counter}")
+    print(f"🔎 Invalid email threads: {invalid_email_counter}")
+
         
 
 
